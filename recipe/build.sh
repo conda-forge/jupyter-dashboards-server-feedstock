bd227e9b15d0680fb7913dfc7aaac6e4ac29d456
Daniel Rodriguez <df.rodriguez143@gmail.com>
2017-02-25 10:54:35 -0600

    Fix missing build of assets (#1)

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-# Some dependencies postinstall scripts (phosphor-dragdrop:`npm dedupe`) causes some permission error on docker
+# --ignore-scripts: Some dependencies postinstall scripts (phosphor-dragdrop:`npm dedupe`)
+# causes some permissions errors when run as root on docker (CI)
+npm install --ignore-scripts
+./node_modules/.bin/gulp build
 npm install -g --ignore-scripts
